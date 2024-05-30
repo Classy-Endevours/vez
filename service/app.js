2496ef105102cf16b998e23427c43070cf0fa59e
Piyush Dubey <75172719+Piyush-Use-Personal@users.noreply.github.com>
2024-05-30 10:26:00 +0530

    added puppeteer linux conf

--- service/app.js
+++ service/app.js
@@ -65,7 +65,19 @@ app.post('/generate-image', async (req, res) => {
 
         const htmlContent = dom.serialize();
 
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // <- this one doesn't works in Windows
+                '--disable-gpu'
+            ]
+        });
         const page = await browser.newPage();
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
